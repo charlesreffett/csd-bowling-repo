9e879834c02a73e5481db1b91138df3bfa4f2b12
Max <maxlandes@maxs-mbp>
2020-06-27 13:01:47 -0400

    Updated with Art and Test Runner Main

--- src/test/java/UserStory1TestRunner.java
+++ src/test/java/UserStory1TestRunner.java
@@ -4,12 +4,28 @@ import org.junit.runner.notification.Failure;
 
 public class UserStory1TestRunner {
 	   public static void main(String[] args) {
+		   
+		   System.out.println("              . --- .");
+		   System.out.println("            /        \\");
+		   System.out.println("           |  O  _  O |");
+		   System.out.println("           |  ./   \\. |");
+		   System.out.println("           /  `-._.-'  \\");
+		   System.out.println("         .' /         \\ `.");
+		   System.out.println("     .-~.-~/           \\~-.~-.");
+		   System.out.println(" .-~ ~    |             |    ~ ~-.");
+		   System.out.println(" `- .     |             |     . -'");
+		   System.out.println("      ~ - |             | - ~");
+		   System.out.println("          \\             / ");
+		   System.out.println("        ___\\           /___");
+		   System.out.println("        ~;_  >- . . -<  _i~");
+		   System.out.println("           `'         `'");
+		   System.out.println("");
 	      Result result = JUnitCore.runClasses(UserStory1Test.class);
 			
 	      for (Failure failure : result.getFailures()) {
 	         System.out.println(failure.toString());
 	      }
-			
+	      System.out.println("Testing Pins Function:");
 	      System.out.println(result.wasSuccessful());
 	   }
 }
